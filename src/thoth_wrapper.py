746deecb2bfa3154b3f4841a446b98b96aebea86
Luca Baffa <luca@openbookpublishers.com>
2021-12-20 12:10:21 +0000

    add add_isbn() method

--- src/thoth_wrapper.py
+++ src/thoth_wrapper.py
@@ -6,6 +6,14 @@ import argparse
 from thothlibrary import ThothClient
 
 RUN_PATH = '/ebook_automation/run'
+AZW3_PATH = '/ebook_automation/output/epub_file.azw3'
+TYPES = {'MOBI', 'AZW3'}
+
+def add_isbn(isbn):
+    cmd = f'ebook-meta {AZW3_PATH} --isbn {isbn}'
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
 
 
 def main():
@@ -20,11 +28,18 @@ def main():
                          stderr=subprocess.PIPE)
     stdout, stderr = p.communicate()
     
-#    doi_url = urljoin('https://doi.org/', args.doi)
+    doi_url = urljoin('https://doi.org/', args.doi)
 
-#    thoth = ThothClient(version="0.6.0")
-#    query = thoth.query('workByDoi',
-#                        {'doi': f'"{doi_url}"'})
+    thoth = ThothClient(version="0.6.0")
+    query = thoth.query('workByDoi',
+                        {'doi': f'"{doi_url}"'})
+
+    for publication in query['publications']:
+        if publication['publicationType'] in TYPES:
+            add_isbn(publication['isbn'])
+            break
+    else:
+        raise ValueError(f'No {",".join(TYPES)} format(s) defined in Thoth')
 
 
 if __name__ == '__main__':
